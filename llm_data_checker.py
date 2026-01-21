35bd3d2ac813333a32748f6a61d2566072f15b00
Luke S (Desktop) <lukesalvidge@proton.me>
2026-01-21 17:37:35 +0000

    Ver 0.1.3 Added basic checker to open CSV in checker

--- llm_data_checker.py
+++ llm_data_checker.py
@@ -1,13 +1,33 @@
 #Libraries
 import pandas as pd
-
+import pathlib 
 
 ########################
 #Data Read in function 
 ########################
 def read_df(path):
-    dataframe = pd.read_csv(path)
-    return dataframe
+    if path.suffix == '.csv':
+        dataframe = pd.read_csv(path)
+        return dataframe
+    else:
+        print("File type not accepted. Please use a CSV file type.")
+
+#advanced path reading
+
+#def read_df_adv(path):
+
+    #reads csv from path
+ #   if p
+
+    #reads xlsx file from path
+  #  elif 
+
+    #reads csv direct
+   # elif 
+
+    #reads xlsx direct
+    #elif 
+
 
 
 ########################
